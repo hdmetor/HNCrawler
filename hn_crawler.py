137b6a8cced340a8d8af883eafe465806f9c3af1
hdmetor <hdmetor@gmail.com>
2014-11-03 23:37:24 -0500

    added arg parsing

--- hn_crawler.py
+++ hn_crawler.py
@@ -3,12 +3,31 @@ from urllib.parse import urlparse
 from urllib.request import Request, urlopen
 from datetime import datetime as dt
 import smtplib
+import argparse
+import datetime
 
-file_loc = 'news.txt'
-your_email = 'something'
+parser = argparse.ArgumentParser()
+parser.add_argument("receivers",help="email address where updates should be sent to", nargs='+')
+parser.add_argument("-sender",help="sender of the email\ndefaults to the first of the receivers")
+parser.add_argument("-output",help="name of the output file\n defaults to month+year", default='')
+parser.add_argument("-email",help="option to send an email in case new jobs have been posted", default=True)
+
+args = parser.parse_args()
+
+
+day = str(datetime.datetime.now().day)
+month = str(datetime.datetime.now().month)
+year = str(datetime.datetime.now().year)
+
+
+if args.output != '':
+    path = args.output + '.html'
+else:
+    path = month + '_' + year + '.html'
+
 root = "http://news.ycombinator.com/item?id="
-id = "8120070"
-
+id = "8542892"
+file_loc = 'appeared_id.txt'
 
 def cool_job(text):
     small_text = text.lower()
@@ -25,7 +44,9 @@ def cool_job(text):
     return any([cond in small_text for cond in conds ])
 
 def create_file (list):
-    with open('jobs.html', 'a') as fp:
+    with open(path, 'a') as fp:
+        title = '<h1>Date added: '+ day + '/ ' + month + '</h1>'
+        fp.write(title)
         for id in list:
             if str(infos[id]['text']) == '':
                 return
@@ -35,11 +56,15 @@ def create_file (list):
 def send_mail(list):
     if list == []:
         return
-    sender = 'hn@gmail.com'
-    receivers = ['lucabelli985@gmail.com']
+    receivers = args.receivers
+    if args.sender:
+        sender = args.sender
+    else:
+        sender = receivers[0]
+
 
     message = """From: HN bot <bot@me.com>
-To: yourself <"""+ your_email +""">
+To: yourself <"""+ sender +""">
 Subject: New jobs form HackerNews
 
 There are """ + str(len(list)) + """ new job posting.
@@ -71,20 +96,14 @@ old_postings = [line.strip() for line in open("news.txt", 'r')]
 infos = {}
 
 for post in posts[4:]:
-    #print('post is ',post)
     if post.img != None and post.img.get('width') == '0':
         links = post.findAll('a')
         if links == []:
         #it is a deleted post
             pass
-        #print(post)
         else:
             index = links[0].get('href').index('&')
             id = links[0].get('href')[9:index]
-        #print(links)
-        ##print('\n\n')
-        #print(post)
-        #print('\n\n')
             user = links[1].get('href')[8:]
             posting_text = post.find('span', {'class':'comment'})
             posting_id.append(id)
@@ -95,10 +114,14 @@ for post in posts[4:]:
 
 new_posting = list(set(posting_id)- set(old_postings))
 
+
+if args.email:
+    send_mail(new_posting)
+
+create_file(new_posting)
+
+#write to the file only if no error were reised
 if new_posting != []:
     with open(file_loc, 'a+') as fp:
         for id in new_posting:
             fp.write(id + '\n')
-send_mail(new_posting)
-create_file(new_posting)
-
