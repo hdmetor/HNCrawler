2c24c6af12723a1f6d975edc1e810a9b57cc326e
hdmetor <hdmetor@gmail.com>
2016-10-05 08:51:24 -0700

    updating code with blog

--- hn_crawler.py
+++ hn_crawler.py
@@ -1,142 +1,72 @@
-from bs4 import BeautifulSoup
 import requests
-from datetime import datetime as dt
-import smtplib
-import argparse
-import datetime
-import re
+from bs4 import BeautifulSoup
+from functools import lru_cache
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import parallel_bulk
 
-parser = argparse.ArgumentParser()
-parser.add_argument("id",help="hackernews id of the post")
-parser.add_argument("-receivers",help="email address where updates should be sent to", nargs='+')
-parser.add_argument("-sender",help="sender of the email\ndefaults to the first of the receivers")
-parser.add_argument("-output",help="name of the output file\n defaults to month+year", default='')
+thread = 12627852
 
-
-args = parser.parse_args()
-
-# local changes
-smtpServer = ""
-old_ids = "old_ids.txt"
-
-day = str(datetime.datetime.now().day)
-month = str(datetime.datetime.now().month)
-year = str(datetime.datetime.now().year)
-
-
-if args.output != '':
-    path = args.output + '.html'
-else:
-    path = month + '_' + year + '.html'
-
-root = "http://news.ycombinator.com/item?id="
-thread = args.id
-
-def whole_word(w):
-    "returns an object if the word is present, None otherwise"
-    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
-
-def cool_job(text):
-    """Given the text of the post, it flags it as interesting or not"""
-    small_text = text.lower()
-    locations = ['SF', 'San Francisco', 'bay', 'bay area', 'ca']
-    subjects = [
-                'data', 'Big Data',
-                'Mathematics', 'Math',
-                'finance', 'financial',
-                'game', 'videogame',
-                'algo', 'algoritmic',
-                'machine learning', 'learning', 'deep learning', 'deeplearning']
-    languages = ['python', 'go', 'mathematica', 'c']
-    conds = [string.lower() for string in subjects + languages]
-
-    return any([whole_word(cond)(small_text) for cond in conds]) and any([whole_word(cond)(small_text) for cond in locations])
-
-def create_file (ids):
-    """Given a list of posting id, write to the file a formatted version of the post"""
-    with open(path, 'a') as fp:
-        title = '<h1>Date added: '+ day + '/ ' + month + '</h1>'
-        fp.write(title)
-        for id in ids:
-            text = str(infos[id]['text'])
-            if not cool_job(text):
-                continue
-            if text == '':
-                return
-            code = '<p><a href=' + root + id  + '>'+ '<h2>' + infos[id]['user'] + '</h2>' +'</a></p>' + '<p>' + str(infos[id]['text']) + '</p>'
-            fp.write(code)
-
-def send_mail(posts):
-    """Given the posts, it send sends an email message to notify about the number of posts.
-    Options for the sender / receiver(s) are specified by the command line
-    """
-    if posts == []:
-        return
-    receivers = args.receivers
-    if args.sender:
-        sender = args.sender
-    else:
-        sender = receivers[0]
-
-    message = """From: HN bot <bot@me.com>
-To: yourself <""" + sender + """>
-Subject: New jobs form HackerNews
-
-There are """ + str(len(posts)) + """ new job posting.
-"""
-
+def clean_text(data):
     try:
-        smtpObj = smtplib.SMTP(smtpServer)
-        smtpObj.sendmail(sender, receivers, message)
-        print ("Successfully sent email")
-    except SMTPException:
-        print ("Error: unable to send email")
+        clean_text = BeautifulSoup(data['text'], "html.parser").text
+        data['text'] = clean_text
+        return data
+    except KeyError:
+          return {}
+
+@lru_cache(maxsize=5000)
+def fetch_hn_data(thread):
+    url = "https://hacker-news.firebaseio.com/v0/item/{}.json".format(thread)
+    r = requests.get(url)
+    assert r.status_code == 200
+    data = r.json()
+    data = clean_text(data)
+    return data
+data = fetch_hn_data(thread)
 
 
+es = Elasticsearch()
+# es.indices.delete(index='hn')
 
-r = requests.get(root  + thread, headers={'User-Agent': 'Mozilla/5.0'})
-assert r.status_code == 200
-soup = BeautifulSoup(r.text, "html.parser")
+mappings = {
+    "mappings" : {
+        "post" : {
+            "properties" : {
+                "time" : {
+                    "type" : "date",
+                    "format" : "strict_date_optional_time||epoch_millis"
+                }
+            }
+        }
+    }
+}
+es.indices.create(index='hn', body=mappings)
 
-posting_id = []
+def format_data_for_action(post_id):
+    data = fetch_hn_data(post_id)
+    #if the text field is not present, the post has been deleted
+    if not data:
+        return {}
+    return {
+        '_index': 'hn',
+        '_type': 'post',
+        '_id': post_id,
+        '_source': data
+    }
 
-posts = soup.findAll('td')[4].findAll('tr')
+acts = [format_data_for_action(r) for r in data['kids'] if format_data_for_action(r)]
+list(parallel_bulk(es, acts))
 
-# This jobs were already considered
-try:
-    old_postings = [line.strip() for line in open(old_ids, 'r')]
-except FileNotFoundError:
-    old_postings = []
+query = {"query" : {
+        "bool" : {
+            "must" : [{
+                "match": {'text' : 'san francisco'}},
+                {"match": {'text' : 'machine learning'}}
+            ]
 
-infos = {}
+        }
 
-for post in posts[4:]:
-    # select a top post only
-    if post.img != None and post.img.get('width') == '0':
-        links = post.findAll('a')
-        if links == []:
-        #it is a deleted post
-            pass
-        else:
-            index = links[0].get('href').index('&')
-            id = links[0].get('href')[9:index]
-            posting_text = post.find('span', {'class':'comment'})
-            if cool_job(str(posting_text)):
-                user = links[1].get('href')[8:]
-                posting_id.append(id)
-                infos[id] = {
-                            'text' : posting_text,
-                            'user' : user}
+    }}
 
-
-new_posting = list(set(posting_id) - set(old_postings))
-
-if args.receivers:
-    send_mail(new_posting)
-create_file(new_posting)
-print("Found ", len(new_posting), " jobs!")
-# archive ids for future use
-if new_posting != []:
-    with open(old_ids, 'a+') as fp:
-        for id in new_posting:
-            fp.write(id + '\n')
+#sanity check
+print(es.search(index='hn', body=query)['hits']['total'])
